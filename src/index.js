466ec629d3ec6759664cfc000b1c6ad70557adca
clara pecot <clara.pecot44@gmail.com>
2024-01-16 14:30:04 +0100

    'app' replaced by 'Hello'

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Hello from './components/hello';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>
 );
 
