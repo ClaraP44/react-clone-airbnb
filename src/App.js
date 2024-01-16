f2e7246650acbe7c5626acc1a9ba4bc11a29bace
clara pecot <clara.pecot44@gmail.com>
2024-01-16 16:08:28 +0100

    array of flats created

--- src/App.js
+++ src/App.js
@@ -14,15 +14,17 @@ class App extends Component {
       "lng": 2.343543
     };
 
+    const flats = [ flat, flat, flat, flat ];
+
     return (
       <div className="app">
         <div className="main">
           <div className="search">
           </div>
           <div className="flats">
-            <Flat flat={flat} />
-            <Flat flat={flat} />
-            <Flat flat={flat} />
+            {flats.map(function(flat) {
+              return <Flat flat={flat}/>
+            })}
           </div>
         </div>
 
