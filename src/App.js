fefae6d42c218a953ee0c06ca5cab66fdf675439
clara pecot <clara.pecot44@gmail.com>
2024-01-18 21:17:54 +0100

    constructor created

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
-  render() {
-    const flat = {
-      "name": "Trendy Apt in Buttes Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-      "price": 200,
-      "priceCurrency": "EUR",
-      "lat": 48.885707,
-      "lng": 2.343543
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
     };
+  }
 
-    const flats = [ flat, flat, flat, flat ];
+  render() {
+    // const flat = {
+    //   "name": "Trendy Apt in Buttes Montmartre",
+    //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+    //   "price": 200,
+    //   "priceCurrency": "EUR",
+    //   "lat": 48.885707,
+    //   "lng": 2.343543
+    // };
+
+    // const flats = [ flat, flat, flat, flat ];
 
     return (
       <div className="app">
@@ -22,7 +29,7 @@ class App extends Component {
           <div className="search">
           </div>
           <div className="flats">
-            {flats.map(function(flat) {
+            {this.state.flats.map((flat) => {
               return <Flat flat={flat}/>
             })}
           </div>
