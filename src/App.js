9ac4b3104d6fec07e41c58adb03955c83dccc62e
clara pecot <clara.pecot44@gmail.com>
2024-02-20 15:03:59 +0100

    bug from key fixed

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
             center={center}
             zoom={11}>
             {this.state.flats.map((flat) => {
-              return <Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />
+              return <Marker lat={flat.lat} lng={flat.lng} text={flat.price} key={flat.name} />
             })}
           </GoogleMapReact>
         </div>
